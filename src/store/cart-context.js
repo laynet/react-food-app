48756a9b300961e1f3771f45dd58fcb6fa99133c
Layne <layneingramtaylor@gmail.com>
2022-03-18 15:20:05 -0500

    initial commit

---
+++ src/store/cart-context.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartContext = React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {},
+    removeItem: (id) => {}
+});
+
+export default CartContext
+
+//we're doing cart-context here and in CartProvider to make the IDE work better
